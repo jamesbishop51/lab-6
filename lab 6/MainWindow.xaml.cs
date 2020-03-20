1a57fc882351372ec47b7fe2c7486dbdca222b98
james bishop <s00187350@mail.itsligo.ie>
2020-03-20 13:45:26 +0000

    finished questions one and two

--- lab 6/MainWindow.xaml.cs
+++ lab 6/MainWindow.xaml.cs
@@ -20,9 +20,43 @@ namespace lab_6
     /// </summary>
     public partial class MainWindow : Window
     {
+        NORTHWNDEntities db = new NORTHWNDEntities();
         public MainWindow()
         {
             InitializeComponent();
+        }
+        // exercise one
+        private void Ex1Button_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from c in db.Categories
+                        join p in db.Products on c.CategoryName equals p.Category.CategoryName
+                        orderby c.CategoryName
+                        select new { Category = c.CategoryName, Product = p.ProductName };
+
+            var result = query.ToList();
+            Ex1LbDisplay.ItemsSource = result;
+
+
+        }
+        // exercise two
+        private void Ex2Button_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from p in db.Products
+                        orderby p.Category.CategoryName, p.ProductName
+                        select new { Category = p.Category.CategoryName, Product = p.ProductName };
+
+            var result = query.ToList();
+
+            Ex2LbDisplay.ItemsSource = result;
+        }
+
+        private void Ex3Button_Click(object sender, RoutedEventArgs e)
+        {
+            var query1 = (from detail in db.Order_Details
+                          where detail.ProductID == 7
+                          select detail);
+
+            int numberOfOrders = query1.Count();
         }
     }
 }
